cdda2be6d6a0e90d6864af772055394b73441959
Dhiraj Mattur <145848438+dhira6j@users.noreply.github.com>
2024-04-26 15:00:18 +0530

    Add files via upload

---
+++ SGPA Calci for 3rd sem.c
@@ -0,0 +1,440 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int s1,s2,s3,s4,s5,s6,s7,s8,s9,total,sgpa,gp,g1,g2,g3,g4,g5,g6,g7,g8,g9;
+ float perc=0,engper;
+ clrscr();
+ printf("M-3 : ",s1);
+ scanf("%d",&s1);
+  if(s1>=90 && s1<=100)
+ {
+  gp=10;
+  g1=3*gp;
+ }
+ else if(s1>=80 && s1<90)
+ {
+  gp=9;
+  g1=3*gp;
+ }
+ else if(s1>=70 && s1<80)
+ {
+  gp=8;
+  g1=3*gp;
+ }
+ else if(s1>=60 && s1<70)
+ {
+  gp=7;
+  g1=3*gp;
+ }
+ else if(s1>=50 && s1<60)
+ {
+  gp=6;
+  g1=3*gp;
+ }
+ else if(s1>=45 && s1<50)
+ {
+  gp=5;
+  g1=3*gp;
+ }
+ else if(s1>=40 && s1<45)
+ {
+  gp=4;
+  g1=3*gp;
+ }
+ else
+ {
+  gp=0;
+  g1=3*gp;
+ }
+ printf("DSA : ",s2);
+ scanf("%d",&s2);
+  if(s2>=90 && s2<=100)
+ {
+  gp=10;
+  g2=4*gp;
+ }
+ else if(s2>=80 && s2<90)
+ {
+  gp=9;
+  g2=4*gp;
+ }
+ else if(s2>=70 && s2<80)
+ {
+  gp=8;
+  g2=4*gp;
+ }
+ else if(s2>=60 && s2<70)
+ {
+  gp=7;
+  g2=4*gp;
+ }
+ else if(s2>=50 && s2<60)
+ {
+  gp=6;
+  g2=4*gp;
+ }
+ else if(s2>=45 && s2<50)
+ {
+  gp=5;
+  g2=4*gp;
+ }
+ else if(s2>=40 && s2<45)
+ {
+  gp=4;
+  g2=4*gp;
+ }
+ else
+ {
+  gp=0;
+  g2=4*gp;
+ }
+ printf("ADE : ",s3);
+ scanf("%d",&s3);
+  if(s3>=90 && s3<=100)
+ {
+  gp=10;
+  g3=3*gp;
+ }
+ else if(s3>=80 && s3<90)
+ {
+  gp=9;
+  g3=3*gp;
+ }
+ else if(s3>=70 && s3<80)
+ {
+  gp=8;
+  g3=3*gp;
+ }
+ else if(s3>=60 && s3<70)
+ {
+  gp=7;
+  g3=3*gp;
+ }
+ else if(s3>=50 && s3<60)
+ {
+  gp=6;
+  g3=3*gp;
+ }
+ else if(s3>=45 && s3<50)
+ {
+  gp=5;
+  g3=3*gp;
+ }
+ else if(s3>=40 && s3<45)
+ {
+  gp=4;
+  g3=3*gp;
+ }
+ else
+ {
+  gp=0;
+  g3=3*gp;
+ }
+ printf("CO : ",s4);
+ scanf("%d",&s4);
+  if(s4>=90 && s4<=100)
+ {
+  gp=10;
+  g4=3*gp;
+ }
+ else if(s4>=80 && s4<90)
+ {
+  gp=9;
+  g4=3*gp;
+ }
+ else if(s4>=70 && s4<80)
+ {
+  gp=8;
+  g4=3*gp;
+ }
+ else if(s4>=60 && s4<70)
+ {
+  gp=7;
+  g4=3*gp;
+ }
+ else if(s4>=50 && s4<60)
+ {
+  gp=6;
+  g4=3*gp;
+ }
+ else if(s4>=45 && s4<50)
+ {
+  gp=5;
+  g4=3*gp;
+ }
+ else if(s4>=40 && s4<45)
+ {
+  gp=4;
+  g4=3*gp;
+ }
+ else
+ {
+  gp=0;
+  g4=3*gp;
+ }
+ printf("SE : ",s5);
+ scanf("%d",&s5);
+  if(s5>=90 && s5<=100)
+ {
+  gp=10;
+  g5=3*gp;
+ }
+ else if(s5>=80 && s5<90)
+ {
+  gp=9;
+  g5=3*gp;
+ }
+ else if(s5>=70 && s5<80)
+ {
+  gp=8;
+  g5=3*gp;
+ }
+ else if(s5>=60 && s5<70)
+ {
+  gp=7;
+  g5=3*gp;
+ }
+ else if(s5>=50 && s5<60)
+ {
+  gp=6;
+  g5=3*gp;
+ }
+ else if(s5>=45 && s5<50)
+ {
+  gp=5;
+  g5=3*gp;
+ }
+ else if(s5>=40 && s5<45)
+ {
+  gp=4;
+  g5=3*gp;
+ }
+ else
+ {
+  gp=0;
+  g5=3*gp;
+ }
+ printf("DMS : ",s6);
+ scanf("%d",&s6);
+  if(s6>=90 && s6<=100)
+ {
+  gp=10;
+  g6=3*gp;
+ }
+ else if(s6>=80 && s6<90)
+ {
+  gp=9;
+  g6=3*gp;
+ }
+ else if(s6>=70 && s6<80)
+ {
+  gp=8;
+  g6=3*gp;
+ }
+ else if(s6>=60 && s6<70)
+ {
+  gp=7;
+  g6=3*gp;
+ }
+ else if(s6>=50 && s6<60)
+ {
+  gp=6;
+  g6=3*gp;
+ }
+ else if(s6>=45 && s6<50)
+ {
+  gp=5;
+  g6=3*gp;
+ }
+ else if(s6>=40 && s6<45)
+ {
+  gp=4;
+  g6=3*gp;
+ }
+ else
+ {
+  gp=0;
+  g6=3*gp;
+ }
+ printf("ADE Lab : ",s7);
+ scanf("%d",&s7);
+  if(s7>=90 && s7<=100)
+ {
+  gp=10;
+  g7=2*gp;
+ }
+ else if(s7>=80 && s7<90)
+ {
+  gp=9;
+  g7=2*gp;
+ }
+ else if(s7>=70 && s7<80)
+ {
+  gp=8;
+  g7=2*gp;
+ }
+ else if(s7>=60 && s7<70)
+ {
+  gp=7;
+  g7=2*gp;
+ }
+ else if(s7>=50 && s7<60)
+ {
+  gp=6;
+  g7=2*gp;
+ }
+ else if(s7>=45 && s7<50)
+ {
+  gp=5;
+  g7=2*gp;
+ }
+ else if(s7>=40 && s7<45)
+ {
+  gp=4;
+  g7=2*gp;
+ }
+ else
+ {
+  gp=0;
+  g7=2*gp;
+ }
+ printf("DSA Lab : ",s8);
+ scanf("%d",&s8);
+  if(s8>=90 && s8<=100)
+ {
+  gp=10;
+  g8=2*gp;
+ }
+ else if(s8>=80 && s8<90)
+ {
+  gp=9;
+  g8=2*gp;
+ }
+ else if(s8>=70 && s8<80)
+ {
+  gp=8;
+  g8=2*gp;
+ }
+ else if(s8>=60 && s8<70)
+ {
+  gp=7;
+  g8=2*gp;
+ }
+ else if(s8>=50 && s8<60)
+ {
+  gp=6;
+  g8=2*gp;
+ }
+ else if(s8>=45 && s8<50)
+ {
+  gp=5;
+  g8=2*gp;
+ }
+ else if(s8>=40 && s8<45)
+ {
+  gp=4;
+  g8=2*gp;
+ }
+ else
+ {
+  gp=0;
+  g8=2*gp;
+ }
+ printf("KAN : ",s9);
+ scanf("%d",&s9);
+  if(s9>=90 && s9<=100)
+ {
+  gp=10;
+  g9=1*gp;
+ }
+ else if(s9>=80 && s9<90)
+ {
+  gp=9;
+  g9=1*gp;
+ }
+ else if(s9>=70 && s9<80)
+ {
+  gp=8;
+  g9=1*gp;
+ }
+ else if(s9>=60 && s9<70)
+ {
+  gp=7;
+  g9=1*gp;
+ }
+ else if(s9>=50 && s9<60)
+ {
+  gp=6;
+  g9=1*gp;
+ }
+ else if(s9>=45 && s9<50)
+ {
+  gp=5;
+  g9=1*gp;
+ }
+ else if(s9>=40 && s9<45)
+ {
+  gp=4;
+  g9=1*gp;
+ }
+ else
+ {
+  gp=0;
+  g9=1*gp;
+ }
+ total=(s1+s2+s3+s4+s5+s6+s7+s8+s9);
+ printf("Total Marks :%d\n",total);
+ sgpa=(g1+g2+g3+g4+g5+g6+g7+g8+g9)/24;
+ printf("SGPA :%d\n",sgpa);
+ engper=((sgpa-0.75)*10);
+ printf("Engg PERCENTAGE : %f\n",engper);
+ perc=total/9;
+ printf("Percentage :%f\n",perc);
+ if((s1>35)&&(s2>35)&&(s3>35)&&(s4>35)&&(s5>35)&&(s6>35)&&(s7>35)&&(s8>35)&&(s9>35))
+	{
+	 printf("you are PASS");
+	 if(engper>=90 && engper<=100)
+	  {
+	   printf("\nYou are OUTSTANDING\nGrade:O");
+	  }
+	  else if(engper>=80 && engper<90)
+	  {
+	   printf("\nYou are EXCELLENT\nGrade:S");
+	  }
+	  else if(engper>=70 && engper<80)
+	  {
+	   printf("\nYou are VERY GOOD\nGrade:A");
+	  }
+	  else if(engper>=60 && engper<70)
+	  {
+	   printf("\nYou are GOOD\nGrade:B");
+	  }
+	  else if(engper>=50 && engper<60)
+	  {
+	   printf("\nYou are ABOVE AVERAGE\nGrade:C");
+	  }
+	  else if(engper>=45 && engper<50)
+	  {
+	   printf("\nYou are AVERAGE\nGrade:E");
+	  }
+	  else if(engper>=40 && engper<45)
+	  {
+	   printf("\nYou are POOR\nGrade:P");
+	  }
+	  else if(engper<35)
+	  {
+	   printf("Better Luck Next Time");
+	  }
+	   else
+	   {
+	    printf("Better Luck Next Time");
+	  }
+	 }
+	  else
+	  {
+	   printf("You are FAIL\nBetter Luck Next Time");
+	  }
+ getch();
+}
+
